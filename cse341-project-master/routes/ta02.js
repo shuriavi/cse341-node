8f5b1d5d8e7ee224022d8cc78ffbe47682cfc7fc
Simi Huriavi <shuriavi@byui.edu>
2022-01-07 14:34:55 -0700

    navigate

--- cse341-project-master/routes/ta02.js
+++ cse341-project-master/routes/ta02.js
@@ -3,9 +3,33 @@
 const express = require('express');
 const router = express.Router();
 
+const userArray = ['Jeremy', 'Tyler', 'Delilah'];
+
+router.post('/addUser', (req, res, next) => {
+  const newUser = req.body.newUser;
+
+  userArray.push(newUser);
+
+  res.redirect('/ta02/');
+});
+
+
+router.post('/removeUser', (req, res, next) => {
+  const remUser = req.body.remUser;
+
+  // Splice method removes from a const array
+  const index = userArray.indexOf(remUser);
+  if (index !== -1) {
+    userArray.splice(index, 1);
+  }
+
+  res.redirect('/ta02/');
+});
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
+    users: userArray,
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
